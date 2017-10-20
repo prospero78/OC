162b5ace545aaf974f0f353088dfa77b62e0244c
prospero78 <pospero.78.su@gmail.com>
2017-10-20 14:28:58 +0200

    01 Создание решения, файла редактора

---
+++ oc/MainForm.vb
@@ -0,0 +1,19 @@
+﻿'
+' Сделано в SharpDevelop.
+' Пользователь: User
+' Дата: 20.10.2017
+' Время: 14:28
+' ${res:XML.StandardHeader.License} ${LICENSE}
+' 
+' Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+'
+Public Partial Class MainForm
+    Public Sub New()
+        ' The Me.InitializeComponent call is required for Windows Forms designer support.
+        Me.InitializeComponent()
+        
+        '
+        ' TODO : Add constructor code after InitializeComponents
+        '
+    End Sub
+End Class
