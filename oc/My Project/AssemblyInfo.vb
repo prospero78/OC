d3a7beb71846b19ca135620f4b258e07c02aa5a6
prospero78 <pospero.78.su@gmail.com>
2017-10-21 15:52:10 +0200

    11 Создан проект в Visual Studio 2017 Community Edition

---
+++ oc/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("oc")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("oc")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("399108a0-8325-40b4-a430-0ed0f6296c62")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
