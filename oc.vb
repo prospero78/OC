f73cfd6ef07cd724fe75c320568191ca533ef20b
prospero78 <pospero.78.su@gmail.com>
2017-10-21 16:51:24 +0200

    15 Добавление цвета в консоли

--- oc.vb
+++ oc.vb
@@ -1,7 +1,26 @@
 ﻿Namespace nsOc
+public class clsWinMain
+end class
 module modOc
+   Dim FoneColor as System.ConsoleColor
+   Sub Color_Write(ByRef txt As String, Optional color as integer = 15)
+      Console.BackgroundColor = System.ConsoleColor.DarkBlue
+      Console.WriteLine(txt)
+      Console.BackgroundColor = FoneColor
+   End Sub
    Sub main()
-   Console.writeline("Oberon-07 compiler")
+      Console.Clear()
+      FoneColor = Console.BackgroundColor
+       
+      'Console.ForegroundColor = 
+      Console.Title = "Oberon-07 Compiler"
+      'Console.writeline("Oberon-07 compiler", System.ConsoleColor.Gray)
+      Color_Write("Oberon-07 compiler")
+      Console.BackgroundColor = System.ConsoleColor.Black
+      Console.ResetColor()
+      Console.WriteLine()
+      Console.Write(">")
+      Console.Read()
    end sub
 end module
 end namespace
