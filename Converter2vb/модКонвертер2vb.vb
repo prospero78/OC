ac1cfc7a2850655d42be3f791d09cfa1b5d752e0
prospero78 <pospero.78.su@gmail.com>
2017-10-21 15:47:09 +0200

    10 Полное удаление

--- Converter2vb/модКонвертер2vb.vb
+++
@@ -1,4 +0,0 @@
-module модКонверт2vb
-
-
-end module
--- Converter2vb/ст_дб.bat
+++
@@ -1,5 +0,0 @@
-echo off
-rem chcp 65001
-cls
-echo Сборка модКонвертер2vb.vb
-vbc /t:library /debug+ /nologo /optionexplicit+ /removeintchecks- /codepage:65001 /platform:x86 /utf8output+ модКонвертер2vb.vb
