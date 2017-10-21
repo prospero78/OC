9add7a207fb44375d6f663b34045c755bbc1cf53
prospero78 <pospero.78.su@gmail.com>
2017-10-21 16:14:38 +0200

    14 Сборка из консоли

---
+++ build.bat
@@ -0,0 +1,4 @@
+﻿@echo off
+cpch 65001
+cls
+vbc /t:exe /debug+ /debug:pdbonly /optionexplicit+ /optioninfer- /rootnamespace:nsOc /optionstrict+ /nologo /platform:x86 /utf8output+ oc.vb
---
+++ oc.vb
@@ -0,0 +1,7 @@
+﻿Namespace nsOc
+module modOc
+   Sub main()
+   Console.writeline("Oberon-07 compiler")
+   end sub
+end module
+end namespace
