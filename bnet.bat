a819b779abc8f5249c0a4ab2435bb114cf5a25b8
prospero78su <prospero.78.su@gmail.com>
2017-10-29 19:44:46 +0200

    57 Начат разбор секции типов. Вынос лексического разбора в отдельный модуль.

--- bnet.bat
+++ bnet.bat
@@ -26,8 +26,11 @@ vbc source\modTagger.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionst
 echo 8. Compile modScaner.dll
 vbc source\modScaner.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:nsOc /platform:x86 /utf8output+ /r:"bin\modArg.dll" /r:"bin\modCocon.dll" /r:"bin\modFile.dll" /r:"bin\modConst.dll"  /t:library /out:"bin\modScaner.dll" /optimize+
 
-echo 9. Compile modCompiler.dll
-vbc source\modCompiler.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:nsOc /platform:x86 /utf8output+ /r:"bin\modConst.dll" /r:"bin\modUtil.dll" /r:"bin\modTagger.dll" /r:"bin\modFile.dll" /r:"bin\modCocon.dll" /t:library /out:"bin\modCompiler.dll" /optimize+
+echo 9. Compile modLexer.dll
+vbc source\modLexer.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:nsOc /platform:x86 /utf8output+  /r:"bin\modTagger.dll" /r:"bin\modUtil.dll" /r:"bin\modCocon.dll" /t:library /out:"bin\modLexer.dll" /optimize+
 
-echo +++ 10. Compile oc.exe +++
-vbc source\oc.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:nsOc /platform:x86 /utf8output+ /r:"bin\modCocon.dll" /r:"bin\modArg.dll" /r:"bin\modFile.dll" /r:"bin\modScaner.dll" /r:"bin\modCompiler.dll" /t:exe /out:"bin\oc.exe" /optimize+
+echo 10. Compile modCompiler.dll
+vbc source\modCompiler.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:nsOc /platform:x86 /utf8output+  /r:"bin\modTagger.dll" /r:"bin\modLexer.dll" /t:library /out:"bin\modCompiler.dll" /optimize+
+
+echo +++ 11. Compile oc.exe +++
+vbc source\oc.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:nsOc /platform:x86 /utf8output+  /r:"bin\modCocon.dll" /r:"bin\modArg.dll" /r:"bin\modFile.dll" /r:"bin\modScaner.dll" /r:"bin\modCompiler.dll" /t:exe /out:"bin\oc.exe" /optimize+
