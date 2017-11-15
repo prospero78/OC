fe5d0ff5c7a928d4bce6ec81a79d7f8a8e53bd07
prospero78 <pospero.78.su@gmail.com>
2017-11-15 09:03:30 +0200

    71 Героическое преодоление косяков

---
+++ GlobalSuppressions.vb
@@ -0,0 +1,16 @@
+﻿
+' This file is used by Code Analysis to maintain SuppressMessage 
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given 
+' a specific target and scoped to a namespace, type, member, etc.
+
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="type", Target:="~T:OC.окнГлавное")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="member", Target:="~M:oc.пиОк.туКоорд.цСтр_Уст(System.Int32)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="member", Target:="~P:oc.пиОк.туКоорд.цСтр")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="member", Target:="~M:oc.пиОк.туКоорд.цПоз_Уст(System.Int32)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="member", Target:="~P:oc.пиОк.туКоорд.цПоз")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="member", Target:="~P:oc.пиОк.тСлова.уПервЛекс")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="type", Target:="~T:oc.пиОк.туСлово")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="type", Target:="~T:oc.пиОк.туКоорд")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="type", Target:="~T:oc.пиОк.туИсход")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Стиль", "IDE1006:Стили именования", Justification:="<Ожидание>", Scope:="type", Target:="~T:oc.пиОк.тСлова")>
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Oberon-07")>
+<Assembly: AssemblyDescription("Compiler Oberon-07 for. Net")>
+<Assembly: AssemblyCompany("KBK Techniks ltd.")>
+<Assembly: AssemblyProduct("Compiler Oberon-07 for. Net")>
+<Assembly: AssemblyCopyright("KBK Techniks ltd.")>
+<Assembly: AssemblyTrademark("(L) BSD-2")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("53e2d11c-ba8c-42b5-9c16-b592b2e96ad2")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
