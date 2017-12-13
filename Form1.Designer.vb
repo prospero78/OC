003f65afd75a5fb68eb8c75a7eb1a86f3ebe2656
prospero78 <prospero.78.su@gmail.com>
2017-12-13 16:07:50 +0200

    92 Попытка использовать словари-2.

---
+++ Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+   Inherits System.Windows.Forms.Form
+
+   'Форма переопределяет dispose для очистки списка компонентов.
+   <System.Diagnostics.DebuggerNonUserCode()> _
+   Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+      Try
+         If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+         End If
+      Finally
+         MyBase.Dispose(disposing)
+      End Try
+   End Sub
+
+   'Является обязательной для конструктора форм Windows Forms
+   Private components As System.ComponentModel.IContainer
+
+   'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+   'Для ее изменения используйте конструктор форм Windows Form.  
+   'Не изменяйте ее в редакторе исходного кода.
+   <System.Diagnostics.DebuggerStepThrough()> _
+   Private Sub InitializeComponent()
+      Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+      Me.ФайлToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+      Me.ОткрытьToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+      Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+      Me.MenuStrip1.SuspendLayout()
+      Me.SuspendLayout()
+      '
+      'MenuStrip1
+      '
+      Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ФайлToolStripMenuItem})
+      Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+      Me.MenuStrip1.Name = "MenuStrip1"
+      Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+      Me.MenuStrip1.TabIndex = 0
+      Me.MenuStrip1.Text = "MenuStrip1"
+      '
+      'ФайлToolStripMenuItem
+      '
+      Me.ФайлToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ОткрытьToolStripMenuItem})
+      Me.ФайлToolStripMenuItem.Name = "ФайлToolStripMenuItem"
+      Me.ФайлToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+      Me.ФайлToolStripMenuItem.Text = "&Файл"
+      '
+      'ОткрытьToolStripMenuItem
+      '
+      Me.ОткрытьToolStripMenuItem.Name = "ОткрытьToolStripMenuItem"
+      Me.ОткрытьToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+      Me.ОткрытьToolStripMenuItem.Text = "&Открыть"
+      '
+      'FlowLayoutPanel1
+      '
+      Me.FlowLayoutPanel1.Location = New System.Drawing.Point(37, 59)
+      Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+      Me.FlowLayoutPanel1.Size = New System.Drawing.Size(40, 39)
+      Me.FlowLayoutPanel1.TabIndex = 1
+      '
+      'Form1
+      '
+      Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+      Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+      Me.ClientSize = New System.Drawing.Size(284, 261)
+      Me.Controls.Add(Me.FlowLayoutPanel1)
+      Me.Controls.Add(Me.MenuStrip1)
+      Me.MainMenuStrip = Me.MenuStrip1
+      Me.Name = "Form1"
+      Me.Text = "Form1"
+      Me.MenuStrip1.ResumeLayout(False)
+      Me.MenuStrip1.PerformLayout()
+      Me.ResumeLayout(False)
+      Me.PerformLayout()
+
+   End Sub
+
+   Friend WithEvents MenuStrip1 As MenuStrip
+   Friend WithEvents ФайлToolStripMenuItem As ToolStripMenuItem
+   Friend WithEvents ОткрытьToolStripMenuItem As ToolStripMenuItem
+   Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+   Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
+
+   End Sub
+End Class
